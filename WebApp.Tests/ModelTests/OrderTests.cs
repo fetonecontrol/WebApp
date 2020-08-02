7521d4de6a3759678790865eabf3f6bb065ea5b0
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-02 14:53:50 -0700

    add working test of order contructor

---
+++ WebApp.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WebApp.Models;
+using System;
+
+
+namespace WebApp.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void Order_StoreOrder_Order()
+    {
+      Order newOrder = new Order("Test", "For Testing", 12);
+      Assert.AreEqual("Test", newOrder.Title);
+      Assert.AreEqual("For Testing", newOrder.Description);
+      Assert.AreEqual(12, newOrder.Date);
+    }
+    
+  }
+}
